652f622b7a919b8f804a26940ce91656b6a4cb1f
Rye Carrigan <rcarrigan@cloudbees.com>
2020-04-23 16:07:44 -0400

    Include state_bucket root

---
+++ eks/roots/state_bucket/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "bucket_name" {}
+
+variable "extra_tags" {
+  default = {}
+  type    = "map"
+}
+
+resource "aws_s3_bucket" "state" {
+  acl    = "private"
+  bucket = var.bucket_name
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = var.extra_tags
+}
