1d43316f0ab611c59c26fa3c1f8cdb61b0569dc9
Rye Carrigan <rcarrigan@cloudbees.com>
2021-10-01 20:35:45 -0400

    Finishing touches

--- roots/eks/efs.tf
+++ roots/eks/efs.tf
@@ -3,8 +3,10 @@ resource "aws_efs_file_system" "efs_file_system" {
 }
 
 resource "aws_efs_mount_target" "efs_mount_target" {
-  for_each        = toset(module.eks_vpc.private_subnets)
+  count      = var.zone_count
+  depends_on = [module.eks_vpc]
+
   file_system_id  = aws_efs_file_system.efs_file_system.id
   security_groups = [aws_security_group.efs.id]
-  subnet_id       = each.value
+  subnet_id       = module.eks_vpc.private_subnets[count.index]
 }
--- roots/eks/vpc.tf
+++ roots/eks/vpc.tf
@@ -1,10 +1,3 @@
-locals {
-  active_zones       = slice(local.availability_zones, 0, var.zone_count)
-  availability_zones = data.aws_availability_zones.available.names
-  private_subnets    = [for i in range(0, var.zone_count) : cidrsubnet(var.cidr_block, 8, 100 +  i)]
-  public_subnets     = [for i in range(0, var.zone_count) : cidrsubnet(var.cidr_block, 8, 200 +  i)]
-}
-
 module "eks_vpc" {
   depends_on = [data.aws_availability_zones.available]
   source     = "terraform-aws-modules/vpc/aws"
@@ -12,9 +5,9 @@ module "eks_vpc" {
 
   name                 = "${var.cluster_name}-vpc"
   cidr                 = var.cidr_block
-  azs                  = local.active_zones
-  private_subnets      = local.private_subnets
-  public_subnets       = local.public_subnets
+  azs                  = slice(data.aws_availability_zones.available.names, 0, var.zone_count)
+  private_subnets      = [for i in range(0, var.zone_count) : cidrsubnet(var.cidr_block, 8, 100 +  i)]
+  public_subnets       = [for i in range(0, var.zone_count) : cidrsubnet(var.cidr_block, 8, 200 +  i)]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
