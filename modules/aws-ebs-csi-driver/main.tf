79e515682f0f028f1591a9da7685b3159ec72e86
Rye Carrigan <rcarrigan@cloudbees.com>
2022-04-02 09:20:17 -0400

    Refactor of EBS driver

--- modules/aws-ebs-csi-driver/main.tf
+++ modules/aws-ebs-csi-driver/main.tf
@@ -1,108 +1,225 @@
-resource "helm_release" "this" {
-  chart      = "aws-ebs-csi-driver"
-  name       = var.release_name
-  namespace  = var.namespace
-  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-  values     = [local.helm_values]
-  version    = var.release_version
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      values   = ["sts.${var.dns_suffix}"]
+      variable = "${var.oidc_issuer}:aud"
+    }
+
+    condition {
+      test     = "StringEquals"
+      values   = ["system:serviceaccount:kube-system:${var.service_account_name}"]
+      variable = "${var.oidc_issuer}:sub"
+    }
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${var.aws_account_id}:oidc-provider/${var.oidc_issuer}"]
+    }
+  }
 }
 
-resource "kubernetes_storage_class" "this" {
-  metadata {
-    name = var.storage_class_name
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class": var.is_default
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions = [
+      "ec2:CreateSnapshot",
+      "ec2:AttachVolume",
+      "ec2:DetachVolume",
+      "ec2:ModifyVolume",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeInstances",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeTags",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeVolumesModifications",
+    ]
+
+    effect    = "Allow"
+    resources = ["*"]
+  }
+
+  statement {
+    actions = ["ec2:CreateTags"]
+    effect  = "Allow"
+
+    resources = [
+      "arn:aws:ec2:*:*:volume/*",
+      "arn:aws:ec2:*:*:snapshot/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      values   = ["CreateVolume", "CreateSnapshot"]
+      variable = "ec2:CreateAction"
     }
   }
 
-  parameters = {
-    encrypted = true
-    type      = "gp2"
+  statement {
+    actions = ["ec2:DeleteTags"]
+    effect  = "Allow"
+
+    resources = [
+      "arn:aws:ec2:*:*:volume/*",
+      "arn:aws:ec2:*:*:snapshot/*",
+    ]
   }
 
-  storage_provisioner = "ebs.csi.aws.com"
+  statement {
+    actions   = ["ec2:CreateVolume"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["true"]
+      variable = "aws:RequestTag/ebs.csi.aws.com/cluster"
+    }
+  }
+
+  statement {
+    actions   = ["ec2:CreateVolume"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["*"]
+      variable = "aws:RequestTag/CSIVolumeName"
+    }
+  }
+
+  statement {
+    actions   = ["ec2:CreateVolume"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["owned"]
+      variable = "aws:RequestTag/kubernetes.io/cluster/*"
+    }
+  }
+
+  statement {
+    actions   = ["ec2:DeleteVolume"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["true"]
+      variable = "ec2:ResourceTag/ebs.csi.aws.com/cluster"
+    }
+  }
+
+  statement {
+    actions   = ["ec2:DeleteVolume"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["*"]
+      variable = "ec2:ResourceTag/CSIVolumeName"
+    }
+  }
+
+  statement {
+    actions   = ["ec2:DeleteVolume"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["owned"]
+      variable = "ec2:ResourceTag/kubernetes.io/cluster/*"
+    }
+  }
+
+  statement {
+    actions   = ["ec2:DeleteSnapshot"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["*"]
+      variable = "ec2:ResourceTag/CSIVolumeSnapshotName"
+    }
+  }
+
+  statement {
+    actions   = ["ec2:DeleteSnapshot"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      values   = ["true"]
+      variable = "ec2:ResourceTag/ebs.csi.aws.com/cluster"
+    }
+  }
 }
 
-resource "aws_iam_policy" "this" {
-  name_prefix = "${var.cluster_name}_ebs-csi-driver"
-  policy      = file("${path.module}/policy.json")
-  tags        = var.extra_tags
+locals {
+  values = yamlencode({
+    controller = {
+      extraVolumeTags = var.volume_tags
+
+      serviceAccount = {
+        annotations = {
+          "eks.amazonaws.com/role-arn" = aws_iam_role.this.arn
+        }
+
+        name = var.service_account_name
+      }
+    }
+
+    enableVolumeSnapshot = true
+
+    image = {
+      repository = "${var.eks_addon_repository}/eks/aws-ebs-csi-driver"
+    }
+
+    storageClasses = [{
+      allowVolumeExpansion = true
+
+      annotations = {
+        "storageclass.kubernetes.io/is-default-class" = tostring(var.is_default_class)
+      }
+
+      name = var.storage_class_name
+
+      parameters = {
+        encrypted = "true"
+        type      = "gp2"
+      }
+    }]
+  })
 }
 
 resource "aws_iam_role" "this" {
-  name_prefix = "${var.cluster_name}_ebs-csi-driver"
-
-  assume_role_policy = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "${var.oidc_provider_arn}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "${var.oidc_issuer}:sub": "system:serviceaccount:${var.namespace}:ebs-csi-controller-sa"
-        }
-      }
-    }
-  ]
-}
-EOT
-
-  tags = var.extra_tags
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  name_prefix        = "${var.cluster_name}_${var.release_name}"
 }
 
-resource "aws_iam_role_policy_attachment" "ebs_policy_attachment" {
+resource "aws_iam_policy" "this" {
+  name_prefix = "${var.cluster_name}_${var.release_name}"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
   policy_arn = aws_iam_policy.this.arn
   role       = aws_iam_role.this.name
 }
 
-locals {
-  ec2_arn_prefix = "arn:aws:ec2:${var.aws_region}:${var.aws_account_id}"
-
-  eks_addon_repository = lookup(local.eks_addon_repository_map, var.aws_region)
-  eks_addon_repository_map = {
-    "af-south-1"     = "877085696533.dkr.ecr.af-south-1.amazonaws.com"
-    "ap-east-1"      = "800184023465.dkr.ecr.ap-east-1.amazonaws.com"
-    "ap-northeast-1" = "602401143452.dkr.ecr.ap-northeast-1.amazonaws.com"
-    "ap-northeast-2" = "602401143452.dkr.ecr.ap-northeast-2.amazonaws.com"
-    "ap-northeast-3" = "602401143452.dkr.ecr.ap-northeast-3.amazonaws.com"
-    "ap-south-1"     = "602401143452.dkr.ecr.ap-south-1.amazonaws.com"
-    "ap-southeast-1" = "602401143452.dkr.ecr.ap-southeast-1.amazonaws.com"
-    "ap-southeast-2" = "602401143452.dkr.ecr.ap-southeast-2.amazonaws.com"
-    "ca-central-1"   = "602401143452.dkr.ecr.ca-central-1.amazonaws.com"
-    "cn-north-1"     = "918309763551.dkr.ecr.cn-north-1.amazonaws.com.cn"
-    "cn-northwest-1" = "961992271922.dkr.ecr.cn-northwest-1.amazonaws.com.cn"
-    "eu-central-1"   = "602401143452.dkr.ecr.eu-central-1.amazonaws.com"
-    "eu-north-1"     = "602401143452.dkr.ecr.eu-north-1.amazonaws.com"
-    "eu-south-1"     = "590381155156.dkr.ecr.eu-south-1.amazonaws.com"
-    "eu-west-1"      = "602401143452.dkr.ecr.eu-west-1.amazonaws.com"
-    "eu-west-2"      = "602401143452.dkr.ecr.eu-west-2.amazonaws.com"
-    "eu-west-3"      = "602401143452.dkr.ecr.eu-west-3.amazonaws.com"
-    "me-south-1"     = "558608220178.dkr.ecr.me-south-1.amazonaws.com"
-    "sa-east-1"      = "602401143452.dkr.ecr.sa-east-1.amazonaws.com"
-    "us-east-1"      = "602401143452.dkr.ecr.us-east-1.amazonaws.com"
-    "us-east-2"      = "602401143452.dkr.ecr.us-east-2.amazonaws.com"
-    "us-gov-east-1"  = "151742754352.dkr.ecr.us-gov-east-1.amazonaws.com"
-    "us-gov-west-1"  = "013241004608.dkr.ecr.us-gov-west-1.amazonaws.com"
-    "us-west-1"      = "602401143452.dkr.ecr.us-west-1.amazonaws.com"
-    "us-west-2"      = "602401143452.dkr.ecr.us-west-2.amazonaws.com"
-  }
-
-  extra_tags = join("\n", [for k, v in var.extra_tags : "\"${k}\": \"${v}\""])
-  helm_values = <<EOT
-controller:
-  extraVolumeTags:
-    ${indent(4, local.extra_tags)}
-  serviceAccount:
-    create: true
-    annotations:
-      eks.amazonaws.com/role-arn: ${aws_iam_role.this.arn}
-enableVolumeSnapshot: true
-image:
-  repository: "${local.eks_addon_repository}/eks/aws-ebs-csi-driver"
-EOT
+resource "helm_release" "this" {
+  chart      = "aws-ebs-csi-driver"
+  name       = var.release_name
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  values     = [local.values]
+  version    = var.release_version
 }
--- modules/aws-ebs-csi-driver/variables.tf
+++ modules/aws-ebs-csi-driver/variables.tf
@@ -1,35 +1,54 @@
-variable "aws_account_id" {}
-variable "aws_region" {}
-variable "cluster_name" {}
-
-variable "extra_tags" {
-  default = {}
-  type    = map(string)
+variable "aws_account_id" {
+  type = string
 }
 
-variable "is_default" {
+variable "aws_region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "dns_suffix" {
+  default = "amazonaws.com"
+  type    = string
+}
+
+variable "eks_addon_repository" {
+  type = string
+}
+
+variable "is_default_class" {
   default = false
   type    = bool
 }
 
-variable "namespace" {
-  default = "kube-system"
+variable "oidc_issuer" {
+  type = string
 }
 
-variable "oidc_issuer" {}
-variable "oidc_provider_arn" {}
 variable "release_name" {
   default = "aws-ebs-csi-driver"
+  type    = string
 }
 
 variable "release_version" {
   default = "2.6.4"
+  type    = string
 }
 
 variable "service_account_name" {
   default = "aws-ebs-csi-driver"
+  type    = string
 }
 
 variable "storage_class_name" {
   default = "ebs"
+  type    = string
+}
+
+variable "volume_tags" {
+  default = {}
+  type    = map(string)
 }
