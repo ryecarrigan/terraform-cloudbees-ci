6c54358ce53fb2ffddf80ce4675a0c86b99abb3c
Rye Carrigan <rcarrigan@cloudbees.com>
2023-08-25 10:02:16 -0400

    Update to latest EKS module

--- roots/eks/main.tf
+++ roots/eks/main.tf
@@ -125,7 +125,7 @@ module "bastion" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+  version = "19.16.0"
 
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
