b3759e6b726dffc1bf1e0f7ea3e8e595c16f4b0f
Rye Carrigan <rcarrigan@cloudbees.com>
2023-09-07 10:26:48 -0400

    Add bastion EIP output

--- modules/aws-bastion/outputs.tf
+++ modules/aws-bastion/outputs.tf
@@ -1,3 +1,7 @@
+output "bastion_eip" {
+  value = aws_eip.this.address
+}
+
 output "security_group_id" {
   value = aws_security_group.this.id
 }
--- roots/eks/outputs.tf
+++ roots/eks/outputs.tf
@@ -1,3 +1,7 @@
+output "bastion_eip" {
+  value = var.bastion_enabled ? module.bastion["this"].bastion_eip : ""
+}
+
 output "kubeconfig_filename" {
   value = local.kubeconfig_file
 }
