81366562c2906212cd71b5a20faa906d5d20c599
Rye Carrigan <rcarrigan@cloudbees.com>
2024-10-10 12:29:11 -0400

    Add output for NAT public IP

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,6 +2,10 @@ output "id" {
   value = module.vpc.vpc_id
 }
 
+output "nat_public_ip" {
+  value = module.vpc.nat_public_ips.0
+}
+
 output "private_subnet_ids" {
   value = module.vpc.private_subnets
 }
